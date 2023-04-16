45afc0ce3102750ff4c166e4e45125da52e24498
unknown <jonathums@gmail.com>
2023-04-16 00:36:25 -0500

    expose fsprovider

--- src/micro.mod.ts
+++ src/micro.mod.ts
@@ -1,4 +1,5 @@
 import Tester from './tester/Tester';
 import Logger from './logger/Logger';
+import FsProvider from './providers/generic/FsProvider';
 
-export { Tester, Logger };
+export { Tester, Logger, FsProvider };
